c343fa1255fe440044f230a01b908eef1d4ed8d8
Joshua Acosta <Acosta.joshua03@gmail.com>
2023-03-31 12:06:06 -0400

    correcting PATH to Path typo

--- NYC_property_sales_dashboard.py
+++ NYC_property_sales_dashboard.py
@@ -10,7 +10,7 @@ st.title("NYC Property Sales between 2003 and 2022")
 
 #Reading data from csv file
 #parent_dir = Path(os.getenv("PARENT_DIR"))
-data_path = PATH("https://github.com/JoshuaAcosta/NYC-property-sales-etl/tree/master/data")
+data_path = Path("https://github.com/JoshuaAcosta/NYC-property-sales-etl/tree/master/data")
 raw_path = data_path.joinpath("raw")
 #stage_path = data_path.joinpath("stage")
 production_path = data_path.joinpath("production")
